9c9333d7f4bd8367dbede106211b26ec0d4dab83
Michelle <xaunyunmichelle.lor@digipen.edu>
2024-02-20 04:51:53 +0800

    initial setup and blue login button

---
+++ frontend/chatroom-app/src/App.tsx
@@ -0,0 +1,11 @@
+import LoginButton from "./components/LoginButton"
+
+function App() {
+  return (
+    <div>
+      <LoginButton>Log In</LoginButton>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/chatroom-app/src/components/LoginButton.tsx
@@ -0,0 +1,18 @@
+interface Props {
+    children: string;
+    // '?' gives color a value is optional, similar to default param in c++
+    // this allows for color to only represent either primary secondary or danger
+    //color?: 'primary' | 'secondary' | 'danger';
+    //color?: string;
+   // onClick: () => void;
+}
+
+const LoginButton = ({ children} : Props) => {
+  return (
+      <button className='btn btn-primary'>
+          {children}
+      </button>
+  )
+}
+
+export default LoginButton
