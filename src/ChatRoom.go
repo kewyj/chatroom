3e4da5ff837ef5aedc4507075e8e2419d123bf10
kewyj <74573283+kewyj@users.noreply.github.com>
2024-02-20 22:04:02 +0800

    polling + listeners

--- src/ChatRoom.go
+++ src/ChatRoom.go
@@ -3,6 +3,7 @@ package src
 import (
 	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/google/uuid"
 )
@@ -52,10 +53,16 @@ func (cr *ChatRoom) GetMessages() *MessageQueue {
 }
 
 func (cr *ChatRoom) SendMessage(msg Message) {
+	if cr.m_messages.Size() > MAX_MESSAGES_IN_ROOM {
+		cr.m_messages.Dequeue()
+	}
 	cr.m_messages.Enqueue(msg)
 }
 
 func (cr *ChatRoom) SendSystemMessage(msg string) {
+	if cr.m_messages.Size() > MAX_MESSAGES_IN_ROOM {
+		cr.m_messages.Dequeue()
+	}
 	cr.m_messages.Enqueue(Message{".:system:.", msg})
 }
 
@@ -64,6 +71,10 @@ type ChatService struct {
 	m_rooms map[string]*ChatRoom
 	// map of username to Chatroom object
 	m_users map[string]*ChatRoom
+	// map of subscribers to unread messages
+	m_subs map[string]*MessageQueue
+	// mutex
+	mu sync.Mutex
 }
 
 func NewChatService() *ChatService {
@@ -73,9 +84,11 @@ func NewChatService() *ChatService {
 		rooms[room.GetID()] = room
 	}
 	users := make(map[string]*ChatRoom)
+	subs := make(map[string]*MessageQueue)
 	return &ChatService{
 		m_rooms: rooms,
 		m_users: users,
+		m_subs:  subs,
 	}
 }
 
@@ -95,10 +108,22 @@ func (cs *ChatService) PrintServerStatus() {
 		}
 		fmt.Println("")
 	}
+	fmt.Println("")
+	fmt.Println("QUEUED MESSAGES")
+	for key, val := range cs.m_subs {
+		fmt.Println("USER: " + key[:4])
+		for _, msg := range *val {
+			fmt.Println("	FROM: " + msg.Username[:4])
+			fmt.Println("	MESSAGE: " + msg.Content)
+		}
+		fmt.Println("")
+	}
 }
 
 func (cs *ChatService) AddUser() (string, error) {
 	// count num rooms
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
 	count := 0
 	name := uuid.New().String()
 	for _, val := range cs.m_rooms {
@@ -107,6 +132,8 @@ func (cs *ChatService) AddUser() (string, error) {
 			// found available room
 			val.AddUser(name)
 			cs.m_users[name] = val
+			cs.m_subs[name] = &MessageQueue{}
+			cs.updateSubscribers(val, Message{".:system:.", name[:4] + " has joined the room."})
 			cs.PrintServerStatus()
 			return name, nil
 		}
@@ -120,15 +147,21 @@ func (cs *ChatService) AddUser() (string, error) {
 	cs.m_rooms[room.GetID()] = room
 	room.AddUser(name)
 	cs.m_users[name] = room
+	cs.m_subs[name] = &MessageQueue{}
+	cs.updateSubscribers(room, Message{".:system:.", name[:4] + " has joined the room."})
+	cs.PrintServerStatus()
 	return name, nil
 }
 
 func (cs *ChatService) SendMessage(msg Message) error {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
 	val, ok := cs.m_users[msg.Username]
 	if ok {
 		// user exists
 		msg.Username = msg.Username[:4] // shorten display name
 		val.SendMessage(msg)
+		cs.updateSubscribers(val, msg)
 		cs.PrintServerStatus()
 		return nil
 	}
@@ -136,15 +169,20 @@ func (cs *ChatService) SendMessage(msg Message) error {
 }
 
 func (cs *ChatService) SendSystemMessage(room string, msg string) error {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
 	val, ok := cs.m_rooms[room]
 	if ok {
 		val.SendSystemMessage(msg)
+		cs.updateSubscribers(val, Message{".:system:.", msg})
 		return nil
 	}
 	return errors.New("ChatService::SendSystemMessage - Room not found")
 }
 
 func (cs *ChatService) SendSystemMessageGlobal(msg string) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
 	for _, val := range cs.m_rooms {
 		val.SendSystemMessage(msg)
 	}
@@ -152,4 +190,14 @@ func (cs *ChatService) SendSystemMessageGlobal(msg string) {
 
 func (cs *ChatService) GetRoom(room string) *ChatRoom {
 	return cs.m_rooms[room]
+}
+
+func (cs *ChatService) updateSubscribers(cr *ChatRoom, msg Message) {
+	for _, user := range cr.m_users {
+		cs.m_subs[user].Enqueue(msg)
+	}
+}
+
+func (cs *ChatService) RetrieveUndelivered(user string) *MessageQueue {
+	return cs.m_subs[user]
 }
--- src/Controller.go
+++ src/Controller.go
@@ -3,4 +3,5 @@ package src
 type Controller interface {
 	AddUser() (string, error)
 	SendMessage(msg Message) error
+	RetrieveUndelivered(string) *MessageQueue
 }
