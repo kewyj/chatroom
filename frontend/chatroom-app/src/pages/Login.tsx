98fd6d7c487dedb57d01a393346047fa8e2d0de8
Michelle <xaunyunmichelle.lor@digipen.edu>
2024-02-21 00:36:22 +0800

    update

--- frontend/chatroom-app/src/pages/Login.tsx
+++ frontend/chatroom-app/src/pages/Login.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LoginButton from '.././components/LoginButton';
+import config from '../../config.json'
+import Axios from "axios";
+import { useState } from "react";
 
-export interface LoginProps {}
+export interface LoginProps { }
 
 const LoginPage: React.FunctionComponent<LoginProps> = (props) => {
+
+    // make state to store the user
+    const [userID, setuserID] = useState("");
+
+    const serverHost = config.server.host;
+    const serverPort = config.server.port;
+    const path = "/newuser";
+    const url = `http://${serverHost}:${serverPort}${path}`;
+
+    useEffect(() => {
+        Axios.get(url).then((respond) => {
+            //console.log(respond.data);
+            setuserID(respond.data);
+        });
+    }, []);
+
     return (
         <div className="LoginPage">
             <LoginButton>Log In</LoginButton>
+            <p>{ userID }</p>
         </div>
     );
 };
