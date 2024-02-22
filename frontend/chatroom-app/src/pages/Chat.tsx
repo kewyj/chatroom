dd3865ef6ec01aab52c785ba27ac6088a032fca0
Michelle <xaunyunmichelle.lor@digipen.edu>
2024-02-23 03:14:12 +0800

    everyt except limit

--- frontend/chatroom-app/src/pages/Chat.tsx
+++ frontend/chatroom-app/src/pages/Chat.tsx
@@ -20,6 +20,10 @@ interface Message {
   content: string;
 }
 
+const isWhitespace = (str: string): boolean => {
+    return /^\s*$/.test(str);
+}
+
 const ChatPage: React.FunctionComponent<ChatProps> = () => {
 
     const dispatch = useDispatch();
@@ -27,10 +31,13 @@ const ChatPage: React.FunctionComponent<ChatProps> = () => {
     const message = useSelector((state: AppState) => state.message);
     const [receivedMessages, setReceivedMessages] = useState<Message[]>([]);
     const usernameToSend = userID ? userID.username : '';
+    const [messageLimit, setMessageLimit] = useState<number>(28);
+    const [isVisible, setVisibility] = useState<boolean>(false);
 
     useEffect(() => {
         // Fetch messages from the server and update receivedMessages state
         fetchMessagesFromServer();
+        //checkSpamFromServer();
 
         // Make interval every 1 sec
         const intervalId = setInterval(fetchMessagesFromServer, 1000);
@@ -50,18 +57,21 @@ const ChatPage: React.FunctionComponent<ChatProps> = () => {
         console.log('Data to send:', dataToSend);
 
         try {
-            // Send message to the server
-            await fetch('http://localhost:3333/chat', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(
-                    dataToSend)
-            });
+            if (!isWhitespace(dataToSend.content)) {
+                // Send message to the server
+                await fetch('http://localhost:3333/chat', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(
+                        dataToSend)
+                });
 
-            // Clear the input field after sending message
-            dispatch(setMessage(''));
+                // Clear the input field after sending message
+                dispatch(setMessage(''));
+            }
+
         } catch (error) {
             console.error('Error sending message:', error);
         }
@@ -72,39 +82,78 @@ const ChatPage: React.FunctionComponent<ChatProps> = () => {
         dispatch(setMessage(value));
     }
 
-const fetchMessagesFromServer = async () => {
-    try {
-        const url = 'http://localhost:3333/poll';
+    const updateReceivedMessages = (data: Message[]) => {
+        setReceivedMessages(prevMessages => {
+            const combinedMessages = [...prevMessages, ...data];
+            const newMessages = combinedMessages.slice(-messageLimit);
+            return newMessages;
+        })
+    }
+
+    // const warnUsers = () => {
+    //     setVisibility(true);
+    // }
+
+    const fetchMessagesFromServer = async () => {
+        try {
+            const url = 'http://localhost:3333/poll';
     
-        const dataforPatch = {
-            username: usernameToSend
-        };
+            const dataforPatch = {
+                username: usernameToSend
+            };
 
-        const response = await fetch(url, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(dataforPatch)
-        });
-        if (!response.ok) {
-            throw new Error('Failed to fetch messages');
+            const response = await fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dataforPatch)
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch messages');
+            }
+            const data = await response.json();
+
+            //console.log(data)
+
+            // CHEck if server response was null before calling state change
+            if (!data)
+                return;
+
+            //console.log(data)
+
+            // Update receivedMessages state with the messages received from the server
+            updateReceivedMessages(data);
         }
-        const data = await response.json();
-
-        //console.log(data)
-
-        // CHEck if server response was null before calling state change
-        if (!data.messages || data.messages.length == 0)
-            return;
-
-        // Update receivedMessages state with the messages received from the server
-        setReceivedMessages(data.messages);
+        catch (error) {
+            console.error('Error fetching messages:', error);
+        }
     }
-    catch (error) {
-        console.error('Error fetching messages:', error);
-    }
-}
+
+    // const checkSpamFromServer = async () => {
+    //     try {
+    //         // change accordingly
+    //         const url = 'http://localhost:3333/poll';
+
+    //         const response = await fetch(url, {
+    //             method: 'GET',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             }
+    //         });
+    //         if (!response.ok) {
+    //             throw new Error('Failed to fetch messages');
+    //         }
+    //         const data = await response.json();
+
+    //         // change accordingly
+    //         if (data.content == "spam")
+    //             warnUsers();
+    //     }
+    //     catch (error) {
+    //         console.error('Error fetching messages:', error);
+    //     }
+    // }
 
     return (
         <div className="container">
@@ -112,18 +161,25 @@ const fetchMessagesFromServer = async () => {
                 <div className="d-flex align-items-center flex-shrink-8 p-3 link-dark text-decoration-none border-bottom">
                     <input className="fs-5 fw-semibold" value={userID?.username?.substring(0, 4) || ''} readOnly />
                 </div>
+                {receivedMessages && receivedMessages.length > 0 && (
+                    <div className="messages-container">
+                        {receivedMessages.map((msg, index) => {
+                            console.log(msg.content);
+                            return (
+                                <div key={index} className="message">
+                                    <strong>{msg.username}: </strong>{msg.content}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
             </div>
             <form onSubmit={send}>
                 <input className="form-control" placeholder="Say something..." value={message} onChange={handleInputChange} />
             </form>
-            <div>
-                {receivedMessages && receivedMessages.map((message, index) => (
-                    <div key={index}>
-                        <div>{message.username}</div>
-                        <div>{message.content}</div>
-                    </div>
-                ))}
-            </div>
+            {isVisible && <div className="spam-message"> 
+                <strong>WARNING : You are spamming!</strong>
+            </div>}
         </div>
     )
 }; 
