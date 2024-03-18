bd8d798e4cb031d39052574f7dbf440f315fb32d
Michelle <xaunyunmichelle.lor@digipen.edu>
2024-03-19 02:19:33 +0800

    change state reset)user will only reset uuid stored not custom username when called

--- frontend/chatroom-ui/src/store.tsx
+++ frontend/chatroom-ui/src/store.tsx
@@ -1,28 +1,35 @@
-import { createStore } from 'redux';
+import { createStore, Action } from 'redux';
 import { SET_MESSAGE } from './action_types';
 import { GET_USER_ID } from './action_types';
 import { RESET_USER } from './action_types';
 import { SET_USERNAME } from './action_types';
 
-// Define an interface for the action
-interface Action {
-  type: string;
-  payload: any;
+interface State {
+  userID: string | null;
+  message: string;
+  username: string;
 }
 
 // Define an initial state value for the app
-const initialState = {
+const initialState: State = {
   userID: null,
   message: '',
   username: ''
 }
 
-function reducer(state = initialState, action : Action) {
+// Define an interface for the action
+type ActionTypes =
+  | { type: typeof GET_USER_ID; payload: string }
+  | { type: typeof RESET_USER }
+  | { type: typeof SET_MESSAGE; payload: string }
+  | { type: typeof SET_USERNAME; payload: string }; 
+
+function reducer(state = initialState, action : ActionTypes) : State {
   switch (action.type) {
     case GET_USER_ID:
       return { ...state, userID: action.payload };
     case RESET_USER:
-      return { userID: null, message: '', username: ''};
+      return { ...state, userID: null, message: ''};
     case SET_MESSAGE:
       return { ...state, message: action.payload };
     case SET_USERNAME:
