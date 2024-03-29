7fbb52cf513ee0a76078fae565ee9be5fb6c8bf7
Michelle <xaunyunmichelle.lor@digipen.edu>
2024-03-29 22:05:28 +0800

    newroom and newuser working the rest not

--- frontend/chatroom-ui/src/components/CreateRoomButton.tsx
+++ frontend/chatroom-ui/src/components/CreateRoomButton.tsx
@@ -2,14 +2,20 @@ import Axios from "axios";
 import React from "react";
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { GET_USER_ID } from "../action_types";
 import { setUsername } from '../actions';
 
 import config from '../config.json';
+import AppState from "../store";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/components/createRoomButton.css'
+
+// Define the type for your store state
+interface AppState {
+  username: string;
+}
 
 interface Props {
   children: string;
@@ -19,6 +25,7 @@ const CreateRoomButton = ({ children }: Props) => {
   const [username, setUsernameState] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const customUsername = useSelector((state: AppState) => state.username);
   
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsernameState(event.target.value);
@@ -27,23 +34,41 @@ const CreateRoomButton = ({ children }: Props) => {
   const handleClick = async () => {
     try {
       // to get userID
-      const serverHost = config.server.host;
-      const serverPort = config.server.port;
-      const nrPath = "/newroom";
-      const nrUrl = `http://${serverHost}:${serverPort}${nrPath}`;
+      // const serverHost = config.server.host;
+      // const serverPort = config.server.port;
+      // const nrPath = "/newroom";
+      // const nrUrl = `http://${serverHost}:${serverPort}${nrPath}`;
 
       // PUT /newroom to create room (implement when database is up)
-      // const roomResponse = await Axios.put(nrUrl);
-      // const roomId = roomResponse.data;
+      const response = await fetch(`https://1bs9qf5xn1.execute-api.ap-southeast-1.amazonaws.com/newroom`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          }
+      });
 
-      // PUT /newuser to get new username
-      const nuPath = "/newuser";
-      const nuUrl = `http://${serverHost}:${serverPort}${nuPath}`;
-      const userResponse = await Axios.put(nuUrl);
-      const newUserID = userResponse.data;
+      const data = await response.json(); 
+      console.log(data.chatroom_id)
+
+      const dataToSend = {
+        custom_username: customUsername,
+        chatroom_id: data.chatroom_id
+      }
+
+      const newUserResponse = await fetch(`https://1bs9qf5xn1.execute-api.ap-southeast-1.amazonaws.com/newuser`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(
+              dataToSend)
+      });
+
+      const newUserID = await newUserResponse.json();
+      console.log(newUserID.user_uuid);
 
       // Dispatch an action to update the store with new userID
-      dispatch({ type: GET_USER_ID, payload: newUserID });
+      //dispatch({ type: GET_USER_ID, payload: newUserID });
 
       navigate('/chat');
     }
